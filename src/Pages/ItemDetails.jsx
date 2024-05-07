99270e05a32df33f0bd4c1b26908fd25ef88c7b5
Akash M Nandan <akash003nandan@gmail.com>
2024-05-08 02:04:04 +0530

    single item api set and connected at itemDetails.jsx

--- src/Pages/ItemDetails.jsx
+++ src/Pages/ItemDetails.jsx
@@ -1,9 +1,46 @@
-// Desc: ItemDetails Page
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import SearchBar from '../Components/SearchBar';
 
 const ItemDetails = () => {
+  const { id } = useParams();
+  const [item, setItem] = useState(null);
+
+  const token = localStorage.getItem('token');
+
+
+  useEffect(() => {
+    axios.get(`https://lordgrim.pythonanywhere.com//api/v1/shop_items/get/${id}`, {
+      headers: {
+         // Use the token from local storage
+        'Authorization': `Bearer ${token}`
+      }
+    
+    })
+      .then((response) => {
+        console.log(response.data.data);
+        setItem(response.data.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, [token, id]);
+
   return (
-    <div>ItemDetails</div>
-  )
+    <div>
+      <SearchBar/>
+      {item ? (
+      <>
+        <h1>{item.name}</h1>
+        <p>{item.description}</p>
+        <p>Price: ${item.unit_price}</p>
+        <p>Created by: {item.created_by}</p>
+        <p>Date uploaded: {item.created_at}</p>
+      </>
+      ) : (<p>Loading...</p>)}
+    </div>
+  );
 }
 
-export default ItemDetails
+export default ItemDetails;
