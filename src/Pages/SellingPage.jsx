70dda2b2605fcbf96361b70b09a8457547738139
Akash M Nandan <akash003nandan@gmail.com>
2024-05-07 20:39:42 +0530

    design set for Selling page

--- src/Pages/SellingPage.jsx
+++ src/Pages/SellingPage.jsx
@@ -31,37 +31,38 @@ const SellingPage = () => {
   };
 
   return (
-    <div>
-      <SearchBar/>
-      <div className='listcr'><p>Create a Listing</p></div>
-      <form onSubmit={handleSubmit}>
-        <div className='imgadd'>
+    <>
+    <SearchBar/>
+    <div className='flex flex-col p-10'>
+      <div className='listcr underline text-4xl p-1 overflow-hidden'><p>Create a Listing</p></div>
+      <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center gap-10 mt-10'>
+        {/* <div className='imgadd'>
            <div className='uploadtext'> <p>Upload the images</p></div>
-            <div className='selecttext'><p>Select all the files together</p></div>
+            <div className='selecttext'><p>Select all the files togther</p></div>
             
             <input type="file" name="image" onChange={handleChange} multiple />
-        </div>
-        <div className='rowvideo'>
+        </div> */}
+        {/* <div className='rowvideo'>
           <div className='videoupload'>
            <div className='videotext'> <p>Upload the videos</p></div>
             <input type="file" name="video" accept="video/*" multiple onChange={handleChange} />
           </div>
-        </div>
+        </div> */}
         <div className='fieldtitle'>
            <label>
-            <div className='titlename'>Title</div>
+            <div className='titlename text-sm'>Title</div>
              <div className='titlesub'>
-            <input type="text" name="title" maxLength={55} className='field' placeholder="Enter title" onChange={handleChange} />
+              <input type="text" name="title" maxLength={55} className='field text-sm p-3 bg-green-50' placeholder="Enter title" onChange={handleChange} />
             </div>
             </label> 
             
         </div>
         <div>
             <label><div className='fieldtitle'>Description</div>
-            <textarea name="description" className='desfield' placeholder=" Enter Material Description" onChange={handleChange} />
+              <textarea name="description" className='desfield text-sm p-3 bg-green-50' placeholder=" Enter Material Description" onChange={handleChange} />
             </label>
         </div>
-        <div>
+        {/* <div>
             <label>
               <div className='fieldtitle'>Category</div>
                <select name="category" className='dropfield'  onChange={handleChange}>
@@ -72,34 +73,34 @@ const SellingPage = () => {
             
               </label>
            
-        </div>
+        </div> */}
        
-          <label>
+          {/* <label>
             <div className='fieldtitle'>Condition</div>
              <select name="condition" className='dropfield'  onChange={handleChange}>
                <option value="">Select condition</option>
                <option value="new">New</option>
                <option value="used">Used</option>
              </select>
-          </label>
-    
-        <div className='fieldtitle'>
-            <label><div className='titlename'>Quantity</div>
-            <div className='titlesub'>
-            <input type="number" name="quantity" placeholder="Enter quantity " onChange={handleChange} />
-            </div>
-            </label>
-            
+          </label> */}
+        <div className='flex gap-4'>
+          <div className='fieldtitle'>
+              <label><div className='titlename'>Quantity</div>
+              <div className='titlesub'>
+              <input type="number" name="quantity" placeholder="Enter quantity" className='bg-green-50 p-2 text-sm rounded-md' onChange={handleChange} />
+              </div>
+              </label>
+              
+          </div>
+          <div className='fieldtitle'>
+              <label><div className='titlename'>Price</div>
+              <div className='titlesub'>
+              <input type="number" name="price" placeholder="Enter Price" className='bg-green-50 p-2 text-sm rounded-md' onChange={handleChange} />
+              </div>
+              </label>
+          </div>
         </div>
-        <div className='fieldtitle'>
-            <label><div className='titlename'>Price</div>
-            <div className='titlesub'>
-            <input type="number" name="price" placeholder=" Enter Price" onChange={handleChange} />
-            </div>
-            </label>
-           
-        </div>
-        <div className='fieldtitle'>
+        {/* <div className='fieldtitle'>
             <label><div className='titlename'>
               Address</div>
               <div className='titlesub'>
@@ -107,12 +108,13 @@ const SellingPage = () => {
               </div>
               </label>
             
-        </div>
+        </div> */}
         <div  >
-          <button className='submitbot' type="submit">Submit</button>
+          <button className='bg-green-300 hover:bg-green-700 hover:text-white p-2 w-28 rounded-2xl transition ease-in-out duration-300' type="submit">Submit</button>
         </div>
       </form>
     </div>
+    </>
   );
 };
 
