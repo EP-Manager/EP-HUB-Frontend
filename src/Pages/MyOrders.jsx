b9a0b21a98813a828fdf33248cd75dfdd21190f1
Akash M Nandan <akash003nandan@gmail.com>
2024-05-08 03:40:56 +0530

    design issue resolved

--- src/Pages/MyOrders.jsx
+++ src/Pages/MyOrders.jsx
@@ -24,19 +24,23 @@ const MyOrders = () => {
 
   return (
     <>
-    <SearchBar/>
-    <div className="flex flex-col items-center justify-center min-h-screen mt-6">
-      {Array.isArray(orders) ? orders.map((order) => (
-        <div key={order.id} className="w-full max-w-md p-4 bg-white rounded shadow-md mb-4">
-          <p className="text-gray-800 text-lg font-semibold">Date: {order.date}</p>
-          <p className="text-gray-600">Total Amount: {order.total_price}</p>
-          <p className="text-gray-600">order-ID {order.item}</p>
-          <p className="text-gray-600">Transaction Type: {order.order_type}</p>
-        </div>
-      )) : null}
-    </div>
+      <SearchBar/>
+      <div className="flex flex-col items-center justify-center min-h-screen mt-6">
+        {Array.isArray(orders) ? orders.map((order) => (
+          <div key={order.id} className="flex w-full max-w-md p-4 bg-white rounded shadow-md mb-4">
+            <div className="flex-1 pr-4">
+              <p className="text-gray-800 text-lg font-semibold">Date: {order.date}</p>
+              <p className="text-gray-600">Order ID: {order.item}</p>
+              <p className="text-gray-600">Total Amount: {order.total_price}</p>
+            </div>
+            <div className="flex-1 pl-4 text-right">
+              <p className="text-gray-600">{order.order_type}</p>
+            </div>
+          </div>
+        )) : null}
+      </div>
     </>
   );
-};
+}
 
 export default MyOrders;
