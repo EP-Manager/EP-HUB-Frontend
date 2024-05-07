689295d81cbe3cecc89d8c3953c07baf7437e105
Akash M Nandan <akash003nandan@gmail.com>
2024-05-08 03:38:26 +0530

    design set for myorders

--- src/Pages/MyOrders.jsx
+++ src/Pages/MyOrders.jsx
@@ -23,17 +23,19 @@ const MyOrders = () => {
   }, [token]);
 
   return (
-    <div>
-      <SearchBar/>
+    <>
+    <SearchBar/>
+    <div className="flex flex-col items-center justify-center min-h-screen mt-6">
       {Array.isArray(orders) ? orders.map((order) => (
-        <div key={order.id}>
-          <p>Date: {order.date}</p>
-          <p>Total Amount: {order.total_price}</p>
-          <p>Plastic Name: {order.item}</p>
-          <p>Transaction Type: {order.order_type}</p>
+        <div key={order.id} className="w-full max-w-md p-4 bg-white rounded shadow-md mb-4">
+          <p className="text-gray-800 text-lg font-semibold">Date: {order.date}</p>
+          <p className="text-gray-600">Total Amount: {order.total_price}</p>
+          <p className="text-gray-600">order-ID {order.item}</p>
+          <p className="text-gray-600">Transaction Type: {order.order_type}</p>
         </div>
       )) : null}
     </div>
+    </>
   );
 };
 
